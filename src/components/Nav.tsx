5b52fd156b6b7672180cf0efe8bb5f99a2ae4cf5
giorgio <giorgiosavron@me.com>
2024-01-25 16:16:17 -0500

    added nav to header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+import { Aboreto } from "next/font/google";
+
+const abor = Aboreto({ weight: "400", subsets: ["latin"] });
+
 export function Nav() {
-  return <nav>.</nav>;
+  return (
+    <nav
+      style={abor.style}
+      className="grid grid-cols-4 pt-2 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:text-center"
+    >
+      <Link href="/about" className="nav-link ">
+        <h2 className={`text-xs md:text-base`}>About </h2>
+      </Link>
+
+      <Link href="/photos" className="nav-link group">
+        <h2 className={`text-xs md:text-base`}>Photos </h2>
+      </Link>
+
+      <Link href="/videos" className="nav-link group">
+        <h2 className={`text-xs md:text-base`}>Videos </h2>
+      </Link>
+
+      <Link href="/book" className="nav-link group">
+        <h2 className={`text-xs md:text-base`}>Book </h2>
+      </Link>
+    </nav>
+  );
 }
