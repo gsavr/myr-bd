df91d9fdb7be8d464fb13306a9501631f2d8ac75
giorgio <giorgiosavron@me.com>
2024-01-25 16:30:09 -0500

    added nav to header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,19 +9,19 @@ export function Nav() {
       style={abor.style}
       className="grid grid-cols-4 pt-2 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:text-center"
     >
-      <Link href="/about" className="nav-link ">
+      <Link href="/about" className="nav-link">
         <h2 className={`text-xs md:text-base`}>About </h2>
       </Link>
 
-      <Link href="/photos" className="nav-link group">
+      <Link href="/photos" className="nav-link">
         <h2 className={`text-xs md:text-base`}>Photos </h2>
       </Link>
 
-      <Link href="/videos" className="nav-link group">
+      <Link href="/videos" className="nav-link">
         <h2 className={`text-xs md:text-base`}>Videos </h2>
       </Link>
 
-      <Link href="/book" className="nav-link group">
+      <Link href="/book" className="nav-link">
         <h2 className={`text-xs md:text-base`}>Book </h2>
       </Link>
     </nav>
