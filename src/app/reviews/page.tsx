c4253144b49bc7b0a8339e6a2abed9ca5cf2a5a5
giorgio <giorgiosavron@me.com>
2024-01-31 13:01:25 -0500

    reviews css

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -4,8 +4,8 @@ import { IconStar } from "@/components/SvgIcons";
 
 export default function Reviews() {
   return (
-    <div className="h-[1800px] w-full">
-      <div className="flex h-[1800px] w-full flex-col items-center">
+    <div className=" w-full">
+      <div className="flex  w-full flex-col items-center">
         <div>
           <h2 className={`${abor.className} py-5 text-4xl`}>Client Reviews</h2>
         </div>
