4e4fb6bcc5f605644dba2115246efabebafa9a03
giorgio <giorgiosavron@me.com>
2024-01-31 12:36:05 -0500

    reviews page

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,4 +1,6 @@
 import { abor } from "@/assets/fonts/fonts";
+import { reviews } from "@/assets/reviews-list";
+import { IconStar } from "@/components/SvgIcons";
 
 export default function Reviews() {
   return (
@@ -6,6 +8,29 @@ export default function Reviews() {
       <div className="flex h-[1800px] w-full flex-col items-center">
         <div>
           <h2 className={`${abor.className} py-5 text-4xl`}>Client Reviews</h2>
+        </div>
+        <div className="flex flex-col">
+          {reviews.map((review) => {
+            return (
+              <div
+                key={review.date}
+                className=" my-3 rounded bg-[#E9EBF1] p-3 text-left shadow-inner"
+              >
+                <div className="flex items-center text-sm">
+                  Client name: {review.reviewer}
+                  <span className="flex pl-3 text-yellow-600">
+                    <IconStar />
+                    <IconStar />
+                    <IconStar />
+                    <IconStar />
+                    <IconStar />
+                  </span>
+                </div>
+                <div className="text-xl font-bold">{review.title}</div>
+                <div>{review.content}</div>
+              </div>
+            );
+          })}
         </div>
 
         <div></div>
---
+++ src/assets/reviews-list.tsx
@@ -0,0 +1,84 @@
+export const reviews = [
+  {
+    reviewer: "Roccio",
+    date: "January 13, 2024",
+    title: "Excellent 👏🏻👏🏻👏🏻 Myrian",
+    content:
+      "Myrian was incredible, I loved it, the guests were fascinated, I will hire her again for the next event…",
+  },
+  {
+    reviewer: "Nedda C.",
+    date: "December 16, 2023",
+    title: "Great Dancer!!",
+    content:
+      "Myrian was fantastic! She entertained the guests with her dance, beautiful costumes, and dancing with sword and Fire! I will definitely hire her again for next event!",
+  },
+  {
+    reviewer: "Amy C.",
+    date: "October 21, 2023",
+    title: "Amazing belly dancer!!!!!",
+    content:
+      "As you read, please excuse all of the exclamation points I'm about to use. I absolutely loved Myrian!!! She is an incredible belly dancer and so professional! We had a black tie wedding and she showed up with a STUNNING wardrobe! She fit in perfectly and really added a special touch to our cocktail hour! If I could repeat my wedding night, I would 1000% hire her again. (I would have loved to have seen her dance with fire, but it wasn't allowed at our venue. She was incredible at dancing with a sword.)",
+  },
+  {
+    reviewer: "Chelsea E.",
+    date: "August 12, 2023",
+    title: "Highly recommend!",
+    content:
+      "Myrian was amazing! Quickly adapts and great at involving the crowd. Her costumes were beautiful, and she was timely. Highly recommend.",
+  },
+  {
+    reviewer: "Chelsea E.",
+    date: "August 11, 2023",
+    title: "Worth Every Penny",
+    content:
+      "Myrian was amazing! Quickly adapts and great at involving the crowd. Her costumes were beautiful, and she was timely. Highly recommend.",
+  },
+  {
+    reviewer: "Mohan",
+    date: "February 11, 2023",
+    title: "Amazing!!! Crowd loved the performance!!",
+    content:
+      "Maryian was on time and had lot of props . Perfectly done for our valentines bash !!! Everyone loved the performance.",
+  },
+  {
+    reviewer: "Wahab",
+    date: "December 17, 2022",
+    title: "Great performance",
+    content: "Very engaging and full of energy. Highly recommended",
+  },
+  {
+    reviewer: "Yuliana S. G.",
+    date: "November 12, 2022",
+    title: "Great Performance!",
+    content:
+      "Myriam was very easy to communicate. She arrives on time and ready to entertain. /ln Everyone enjoyed her energy and personality. /ln Her show was amazing. Such great feedback from all of my guests. I will definitely call her again for my next event!",
+  },
+  {
+    reviewer: "Suthisa J.",
+    date: "November 6, 2022",
+    title: "She is five star",
+    content:
+      "We have Myrian for my motoher in Law birthday party. Her performance is absolutely great. She is so friendly , beautiful, loyalty. I would recommend her and give her 5 star!",
+  },
+  {
+    reviewer: "Lisette A.",
+    date: "November 5, 2022",
+    title: "Crowd favorite!",
+    content:
+      "She's beautiful, was on time, flexible & great dancer. She engaged our group & we loved her! We loved her hubby too :-)",
+  },
+  {
+    reviewer: "Francisco O.",
+    date: "September 10, 2022",
+    title: "Great addition to my wife's 40th.",
+    content:
+      "Myrian was great. Very easy to work with and ensure that everyone had fun. She dances very well. I would definitely use Myrian in the future.",
+  },
+  {
+    reviewer: "Mobina E.",
+    date: "August 27, 2022",
+    title: "Amazing",
+    content: "She is beautiful talented friendly and amazing performance.",
+  },
+];
