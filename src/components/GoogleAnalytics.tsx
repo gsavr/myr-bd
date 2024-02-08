12541c2267b03c8753a2b5da8d81bd39efd2850a
giorgio <giorgiosavron@me.com>
2024-02-08 13:31:03 -0500

    google tag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -4,7 +4,7 @@ export const GoogleAnalyticsTracking = () => {
   return (
     <>
       {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-GOOGLEID" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-16453682451" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
